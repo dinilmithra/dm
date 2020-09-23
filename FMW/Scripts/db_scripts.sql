f5cfad396b27ed555aa1467b567b7fd96eef2af7
dinilmithra <vs.dinilmithra@gmail.com>
2020-09-23 18:21:06 -0400

    Update db_scripts.sql

--- FMW/Scripts/db_scripts.sql
+++ FMW/Scripts/db_scripts.sql
@@ -5,3 +5,12 @@ alter system set open_cursors=1000 scope=both;
 alter session set container=cdb$root;
 select name, con_id, value from v$system_parameter where name='open_cursors';
 shutdown immediate;
+
+select name, open_mode from v$pdbs;
+
+ALTER PLUGGABLE DATABASE ORCLPDB OPEN;
+alter pluggable database all open;
+
+alter pluggable database ORCLPDB save state;
+alter pluggable database all save state;
+alter pluggable database all except pdb_name1, pdb_name2 save state;
