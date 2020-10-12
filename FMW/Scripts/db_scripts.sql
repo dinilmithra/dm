5f8a7f4d27a15c7398bbbcc73223d4bb9a89b566
dinilmithra <vs.dinilmithra@gmail.com>
2020-10-11 22:46:23 -0400

    Update db_scripts.sql

--- FMW/Scripts/db_scripts.sql
+++ FMW/Scripts/db_scripts.sql
@@ -14,3 +14,6 @@ alter pluggable database all open;
 alter pluggable database ORCLPDB save state;
 alter pluggable database all save state;
 alter pluggable database all except pdb_name1, pdb_name2 save state;
+
+show parameter open_cursors;
+alter system set open_cursors=1000 scope=both;
